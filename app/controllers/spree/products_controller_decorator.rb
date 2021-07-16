1cd5ea744cb9a73b8a58b252bc7259f070f567e1
umairrazam <umairrazam@gmail.com>
2021-07-16 21:06:18 +0500

    fix(reviews): text rating

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   module ProductsControllerDecorator
     def self.prepended(base)
-      base.helper Spree::ReviewsHelper
+      base.helper ::Spree::ReviewsHelper
     end
 
     ::Spree::ProductsController.prepend self if ::Spree::ProductsController.included_modules.exclude?(self)
